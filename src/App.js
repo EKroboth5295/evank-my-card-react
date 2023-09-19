7ae53924a9504a2ca49479bb3b83a5a86840ca7e
EKroboth5295 <eck5295@psu.edu>
2023-09-19 13:07:56 -0400

    got card to work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><div class="buttons">
+      <button class="duplicate">Duplicate</button>
+    
+      <button class="changeHeading">Change Heading</button>
+    
+      <button id="deleteInstance">Delete Last Instance</button>
+    
+      <button data-toggle-btn>Toggle Details</button>
+    
     </div>
+    <div class="wrapper">
+        <div class="container">
+          <img class="image" src="https://mcdn.wallpapersafari.com/medium/71/81/0VgmpU.jpg" />
+          <div class="header">
+            <h1>My Card: Mario</h1>
+          </div>
+    
+          <details class="details">
+            <summary>Details</summary>
+            <div class="desc-txt">
+              <p>My card is displaying one of my favorite video game characters ever, Mario. I chose this character because it is a character that means a lot to me through my life. This card design has the title at the top, displayed in the top-center of the card, with a photo beneath it. The photo itself displays mario jumping upwards, with a light blue background that contrasts well with the rest of the card.</p>
+            </div>
+          </details>
+        </div>
+      </div>
+      </>
   );
 }
 
